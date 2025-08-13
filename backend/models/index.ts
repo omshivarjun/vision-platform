9bc4e9d77fbaddc0f065996618880b5e8c3cdefc
omshivarjun <123530518+omshivarjun@users.noreply.github.com>
2025-08-13 17:10:42 +0530

    Deleted User.ts

--- backend/models/index.ts
+++
@@ -1,5 +0,0 @@
-// Export all models from the backend directory
-export { User, type IUser, type UserDocument, type UserProfile } from './User';
-
-// Re-export types for convenience
-export type { IUser as UserInterface } from './User';
